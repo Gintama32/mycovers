60c4346234284503f5bb24192df71b4da236d9f4
Gintama32 <107152823+Gintama32@users.noreply.github.com>
2023-04-08 10:59:35 -0500

    initial file 1.0

---
+++ index.js
@@ -0,0 +1,17 @@
+let nextPageToken = "";
+function getVideos(){
+fetch("https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=UCjbcEPQmTWImT7sCwLu3JUg&maxResults=10&order=date&key=AIzaSyDrEuNjJQvwvzv-lg9TI7Yg9K_5MRi7LBA&pageToken="+nextPageToken)
+.then((result)=>{
+    return result.json()
+}).then((data=>{
+    let videos = data.items;
+    let videoContainer = document.querySelector(".youtube-container");
+    for(video of videos){
+        videoContainer.innerHTML+= `
+        <h3>${video.snippet.title}</h3>
+        <img src="${video.snippet.thumbnails.high.url}">
+        `
+    }
+}))
+}
+getVideos();
